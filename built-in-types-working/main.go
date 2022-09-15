4bf99fcad06c676e90d409c0fb2d041c083673da
Roy Christian Pery <thercpery@gmail.com>
2022-09-15 17:10:03 +0800

    added maps channels functions and interfaces

--- built-in-types-working/main.go
+++ built-in-types-working/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"sort"
+	// "github.com/eiannone/keyboard"
 )
 
 // basic types (numbers, strings, booleans)
@@ -34,8 +35,86 @@ type Car struct {
 // reference types (pointers, slices, maps, functions, channels)
 
 // interface type
+type Animal interface {
+	Says() string
+	HowManyLegs() int
+}
+
+// Dog is the type for dogs
+type Dog struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+func (d *Dog) Says() string {
+	return d.Sound
+}
+
+func (d *Dog) HowManyLegs() int {
+	return d.NumberOfLegs
+}
+
+type Cat struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+	HasTail      bool
+}
+
+func (d *Cat) Says() string {
+	return d.Sound
+}
+
+func (d *Cat) HowManyLegs() int {
+	return d.NumberOfLegs
+}
+
+/* type Animal struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+*/
+/* func (a *Animal) Says() {
+	fmt.Printf("A %s says %s\n", a.Name, a.Sound)
+}
+
+func (a *Animal) HowManyLegs() {
+	fmt.Printf("A %s has %d legs\n", a.Name, a.NumberOfLegs)
+}
+*/
+var keyPressChan chan rune
 
 func main() {
+	/* 	go doSomething("Hello, world")
+	   	fmt.Println("This is another message")
+
+	   	for {
+	   		// do nothing
+	   	}
+	*/
+
+	/* keyPressChan = make(chan rune)
+
+	go listenForKeyPress()
+
+	fmt.Println("Press any key, or q to quit.")
+	_ = keyboard.Open()
+
+	defer func() {
+		keyboard.Close()
+	}()
+
+	for {
+		char, _, _ := keyboard.GetSingleKey()
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		keyPressChan <- char
+	}
+	*/
 	myInt = 10
 	myUint = 20
 
@@ -123,6 +202,74 @@ func main() {
 
 	animals = DeleteFromSlice(animals, 1)
 	fmt.Println(animals)
+
+	intMap := make(map[string]int)
+	intMap["one"] = 1
+	intMap["two"] = 2
+	intMap["three"] = 3
+	intMap["four"] = 4
+	intMap["five"] = 5
+
+	for key, value := range intMap {
+		fmt.Println(key, value)
+	}
+
+	// delete(intMap, "four")
+
+	for key, value := range intMap {
+		fmt.Println(key, value)
+	}
+
+	el, ok := intMap["four"]
+
+	if ok {
+		fmt.Println(el, "is in map")
+	} else {
+		fmt.Println(el, "is in not map")
+	}
+
+	intMap["two"] = 4
+	fmt.Println(intMap["two"])
+
+	z := addTwoNumbers(2, 4)
+	fmt.Println(z)
+
+	myTotal := sumMany(2, 3, 4, 5, 88, 7, -5)
+	fmt.Println(myTotal)
+
+	/* var dog Animal
+	dog.Name = "dog"
+	dog.Sound = "woof"
+	dog.NumberOfLegs = 4
+
+	dog.Says()
+
+	cat := Animal{
+		Name:         "cat",
+		Sound:        "meow",
+		NumberOfLegs: 4,
+	}
+
+	cat.Says()
+	cat.HowManyLegs()
+	*/
+
+	dog := Dog{
+		Name:         "dog",
+		Sound:        "woof",
+		NumberOfLegs: 4,
+	}
+
+	Riddle(&dog)
+
+	var cat Cat
+	cat.Name = "cat"
+	cat.NumberOfLegs = 4
+	cat.Sound = "meow"
+	cat.HasTail = true
+
+	Riddle(&cat)
+
 }
 
 func changeValueOfPointer(num *int) {
@@ -134,4 +281,41 @@ func DeleteFromSlice(a []string, i int) []string {
 	a[len(a)-1] = ""
 	a = a[:len(a)-1]
 	return a
+}
+
+func addTwoNumbers(x, y int) (sum int) {
+	sum = x + y
+	return
+}
+
+func sumMany(nums ...int) int {
+	total := 0
+	for _, x := range nums {
+		total = total + x
+	}
+
+	return total
+}
+
+func doSomething(s string) {
+	until := 0
+	for {
+		fmt.Println("s is", s)
+		until = until + 1
+		if until >= 5 {
+			break
+		}
+	}
+}
+
+func listenForKeyPress() {
+	for {
+		key := <-keyPressChan
+		fmt.Println("You pressed", string(key))
+	}
+}
+
+func Riddle(a Animal) {
+	riddle := fmt.Sprintf(`This animal says %s and has %d legs. What animal is it?`, a.Says(), a.HowManyLegs())
+	fmt.Println(riddle)
 }
